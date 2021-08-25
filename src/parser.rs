c067940d0fa400cd845cd78390e8b08768763241
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-08-25 18:06:08 +1000

    fix: remove leading tabs

--- src/parser.rs
+++ src/parser.rs
@@ -707,7 +707,7 @@ impl DocParser {
       }
     }
 
-    let js_doc_re = Regex::new(r#" ?\* ?"#).unwrap();
+    let js_doc_re = Regex::new(r#"\t* ?\* ?"#).unwrap();
     let txt = js_doc_comment
       .text
       .split('\n')
