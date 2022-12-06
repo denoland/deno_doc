3ef0b862732d3195660b23d0d30e5b2a522726e8
David Sherret <dsherret@users.noreply.github.com>
2022-12-06 11:16:11 -0500

    feat: support re-exported json modules (#309)

--- src/tests.rs
+++ src/tests.rs
@@ -855,7 +855,6 @@ async fn exports_imported_earlier_private() {
   let entries = DocParser::new(graph, true, analyzer.as_capturing_parser())
     .parse_with_reexports(&specifier)
     .unwrap();
-  assert_eq!(entries.len(), 2);
 
   let expected_json = json!([
     {
@@ -910,6 +909,134 @@ async fn variable_syntax() {
   DocParser::new(graph, false, analyzer.as_capturing_parser())
     .parse_with_reexports(&specifier)
     .unwrap();
+}
+
+#[tokio::test]
+async fn json_module() {
+  let (graph, analyzer, specifier) = setup(
+    "file:///foo.ts",
+    vec![
+      ("file:///foo.ts", None, "export { default as configFile } from './bar.json' assert { type: 'json' };"),
+      ("file:///bar.json", None, r#"{ "a": 5, "b": "text", "c": null, "d": [1, 2], "e": { "a": 1 } }"#),
+    ],
+  )
+  .await;
+
+  let entries = DocParser::new(graph, false, analyzer.as_capturing_parser())
+    .parse_with_reexports(&specifier)
+    .unwrap();
+
+  let expected_json = json!([
+    {
+      "kind": "variable",
+      "name": "configFile",
+      "location": {
+        "filename": "file:///bar.json",
+        "line": 1,
+        "col": 0
+      },
+      "declarationKind": "export",
+      "variableDef": {
+        "tsType": {
+          "repr": "",
+          "kind": "typeLiteral",
+          "typeLiteral": {
+            "methods": [],
+            "properties": [{
+              "name": "a",
+              "params": [],
+              "computed": false,
+              "optional": false,
+              "tsType": {
+                "repr": "5",
+                "kind": "literal",
+                "literal": {
+                  "kind": "number",
+                  "number": 5.0,
+                },
+              },
+              "typeParams": []
+            }, {
+              "name": "b",
+              "params": [],
+              "computed": false,
+              "optional": false,
+              "tsType": {
+                "repr": "text",
+                "kind": "literal",
+                "literal": {
+                  "kind": "string",
+                  "string": "text",
+                },
+              },
+              "typeParams": []
+            }, {
+              "name": "c",
+              "params": [],
+              "computed": false,
+              "optional": false,
+              "tsType": {
+                "repr": "null",
+                "kind": "keyword",
+                "keyword": "null",
+              },
+              "typeParams": []
+            }, {
+              "name": "d",
+              "params": [],
+              "computed": false,
+              "optional": false,
+              "tsType": {
+                "repr": "unknown[]",
+                "kind": "array",
+                "array": {
+                  "repr": "unknown",
+                  "kind": "keyword",
+                  "keyword": "unknown",
+                },
+              },
+              "typeParams": []
+            }, {
+              "name": "e",
+              "params": [],
+              "computed": false,
+              "optional": false,
+              "tsType": {
+                "repr": "",
+                "kind": "typeLiteral",
+                "typeLiteral": {
+                  "methods": [],
+                  "properties": [{
+                    "name": "a",
+                    "params": [],
+                    "computed": false,
+                    "optional": false,
+                    "tsType": {
+                      "repr": "1",
+                      "kind": "literal",
+                      "literal": {
+                        "kind": "number",
+                        "number": 1.0,
+                      },
+                    },
+                    "typeParams": []
+                  }],
+                  "callSignatures": [],
+                  "indexSignatures": [],
+                },
+              },
+              "typeParams": []
+            }],
+            "callSignatures": [],
+            "indexSignatures": [],
+          },
+        },
+        "kind": "var",
+      },
+    },
+  ]);
+  let actual = serde_json::to_value(&entries).unwrap();
+  assert_eq!(actual, expected_json);
 }
 
 mod serialization {
@@ -2246,7 +2373,7 @@ export const tpl2 = `Value: ${num}`;
       /** other doc */
       b: number;
     } = c;
-    
+
     export const { f, g: h } = d;
     "#;
     [
@@ -4444,7 +4571,7 @@ export function foo(bar: any): asserts bar {}
         return "h";
       },
       set h(value: string) {
-    
+
       },
       [s]: [1, 2, 3, "a"],
       [t](u: string): void {},
