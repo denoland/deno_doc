ce697b68d69ab4399de7f0a26f7a199f3a034156
David Sherret <dsherret@gmail.com>
2023-10-25 19:13:47 -0400

    Add back example

---
+++ examples/ddoc/main.rs
@@ -0,0 +1,95 @@
+// Copyright 2020-2022 the Deno authors. All rights reserved. MIT license.
+
+use clap::App;
+use clap::Arg;
+use deno_doc::find_nodes_by_name_recursively;
+use deno_doc::DocNodeKind;
+use deno_doc::DocParser;
+use deno_doc::DocPrinter;
+use deno_graph::source::LoadFuture;
+use deno_graph::source::LoadResponse;
+use deno_graph::source::Loader;
+use deno_graph::BuildOptions;
+use deno_graph::CapturingModuleAnalyzer;
+use deno_graph::GraphKind;
+use deno_graph::ModuleGraph;
+use deno_graph::ModuleSpecifier;
+use futures::executor::block_on;
+use futures::future;
+use std::env::current_dir;
+use std::fs::read_to_string;
+
+struct SourceFileLoader {}
+
+impl Loader for SourceFileLoader {
+  fn load(
+    &mut self,
+    specifier: &ModuleSpecifier,
+    _is_dynamic: bool,
+    _cache_setting: deno_graph::source::CacheSetting,
+  ) -> LoadFuture {
+    let result = if specifier.scheme() == "file" {
+      let path = specifier.to_file_path().unwrap();
+      read_to_string(path)
+        .map(|content| {
+          Some(LoadResponse::Module {
+            specifier: specifier.clone(),
+            maybe_headers: None,
+            content: content.into(),
+          })
+        })
+        .map_err(|err| err.into())
+    } else {
+      Ok(None)
+    };
+    Box::pin(future::ready(result))
+  }
+}
+
+async fn run() -> anyhow::Result<()> {
+  let matches = App::new("ddoc")
+    .arg(Arg::with_name("source_file").required(true))
+    .arg(Arg::with_name("filter"))
+    .get_matches();
+  let source_file = matches.value_of("source_file").unwrap();
+  let maybe_filter = matches.value_of("filter");
+  let source_file =
+    ModuleSpecifier::from_directory_path(current_dir().unwrap())
+      .unwrap()
+      .join(source_file)
+      .unwrap();
+  let mut loader = SourceFileLoader {};
+  let analyzer = CapturingModuleAnalyzer::default();
+  let mut graph = ModuleGraph::new(GraphKind::TypesOnly);
+  graph
+    .build(
+      vec![source_file.clone()],
+      &mut loader,
+      BuildOptions {
+        module_analyzer: Some(&analyzer),
+        ..Default::default()
+      },
+    )
+    .await;
+  let parser = DocParser::new(graph, false, analyzer.as_capturing_parser())?;
+  let mut doc_nodes = parser.parse_with_reexports(&source_file)?;
+
+  doc_nodes.retain(|doc_node| doc_node.kind != DocNodeKind::Import);
+  if let Some(filter) = maybe_filter {
+    doc_nodes = find_nodes_by_name_recursively(doc_nodes, filter.to_string());
+  }
+  let result = DocPrinter::new(&doc_nodes, true, false);
+  println!("{}", result);
+  Ok(())
+}
+
+fn main() {
+  let future = async move {
+    if let Err(err) = run().await {
+      eprintln!("{}", err);
+      std::process::exit(1);
+    }
+  };
+
+  block_on(future);
+}
