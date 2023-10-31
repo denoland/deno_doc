79de851dae627572fe3f520d5be389dc13ce5028
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-10-31 23:19:10 +0100

    update HTML title

--- src/html/mod.rs
+++ src/html/mod.rs
@@ -313,7 +313,7 @@ fn render_compound_index(
     &json!({
       // TODO(bartlomieju): dedup with `render_page`
       "html_head": {
-        "title": format!("Index - {}", ctx.package_name),
+        "title": format!("Index - {} documentation", ctx.package_name),
         "additional_css": additional_css,
         "stylesheet_url": format!("./{}", STYLESHEET_FILENAME),
       },
@@ -351,7 +351,7 @@ fn render_index(
     &json!({
       // TODO(bartlomieju): dedup with `render_page`
       "html_head": {
-        "title": format!("Index - {}", ctx.package_name),
+        "title": format!("Index - {} documentation", ctx.package_name),
         "additional_css": additional_css,
         "stylesheet_url": format!("./{}", STYLESHEET_FILENAME),
       },
@@ -452,7 +452,7 @@ fn render_page(
     &json!({
       // TODO(bartlomieju): dedup with `render_index`
       "html_head": {
-        "title": format!("{} - {}", name, ctx.package_name),
+        "title": format!("{} - {} documentation", name, ctx.package_name),
         "additional_css": additional_css,
         "stylesheet_url": format!("./{backs}{STYLESHEET_FILENAME}"),
       },
