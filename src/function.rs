ce71bb319d52b345e3483b4a22522155be092538
David Sherret <dsherret@users.noreply.github.com>
2023-10-31 18:03:37 -0400

    feat: infer void return types more (#383)

--- src/function.rs
+++ src/function.rs
@@ -9,6 +9,8 @@ use crate::ts_type::TsTypeDef;
 use crate::ts_type_param::maybe_type_param_decl_to_type_param_defs;
 use crate::ts_type_param::TsTypeParamDef;
 use crate::ParamDef;
+use deno_ast::swc::ast::ReturnStmt;
+use deno_ast::swc::ast::Stmt;
 use deno_ast::ParsedSource;
 use serde::{Deserialize, Serialize};
 
@@ -37,7 +39,29 @@ pub fn function_to_function_def(
     .collect();
 
   let maybe_return_type =
-    function.return_type.as_deref().map(ts_type_ann_to_def);
+    match function.return_type.as_deref().map(ts_type_ann_to_def) {
+      Some(return_type) => Some(return_type),
+      None
+        if !function.is_generator
+          && function.body.is_some()
+          && get_return_stmt_from_function(function).is_none() =>
+      {
+        if function.is_async {
+          Some(TsTypeDef {
+            repr: "Promise".to_string(),
+            kind: Some(crate::ts_type::TsTypeDefKind::TypeRef),
+            type_ref: Some(crate::ts_type::TsTypeRefDef {
+              type_params: Some(vec![TsTypeDef::keyword("void")]),
+              type_name: "Promise".to_string(),
+            }),
+            ..Default::default()
+          })
+        } else {
+          Some(TsTypeDef::keyword("void"))
+        }
+      }
+      None => None,
+    };
 
   let type_params =
     maybe_type_param_decl_to_type_param_defs(function.type_params.as_deref());
@@ -64,4 +88,58 @@ pub fn get_doc_for_fn_decl(
   let name = fn_decl.ident.sym.to_string();
   let fn_def = function_to_function_def(parsed_source, &fn_decl.function);
   (name, fn_def)
+}
+
+fn get_return_stmt_from_function(
+  func: &deno_ast::swc::ast::Function,
+) -> Option<&ReturnStmt> {
+  let body = func.body.as_ref()?;
+  get_return_stmt_from_stmts(&body.stmts)
+}
+
+fn get_return_stmt_from_stmts(stmts: &[Stmt]) -> Option<&ReturnStmt> {
+  for stmt in stmts {
+    if let Some(return_stmt) = get_return_stmt_from_stmt(stmt) {
+      return Some(return_stmt);
+    }
+  }
+
+  None
+}
+
+fn get_return_stmt_from_stmt(stmt: &Stmt) -> Option<&ReturnStmt> {
+  match stmt {
+    Stmt::Block(n) => get_return_stmt_from_stmts(&n.stmts),
+    Stmt::With(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::Return(n) => Some(n),
+    Stmt::Labeled(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::If(n) => get_return_stmt_from_stmt(&n.cons),
+    Stmt::Switch(n) => n
+      .cases
+      .iter()
+      .find_map(|case| get_return_stmt_from_stmts(&case.cons)),
+    Stmt::Try(n) => get_return_stmt_from_stmts(&n.block.stmts)
+      .or_else(|| {
+        n.handler
+          .as_ref()
+          .and_then(|h| get_return_stmt_from_stmts(&h.body.stmts))
+      })
+      .or_else(|| {
+        n.finalizer
+          .as_ref()
+          .and_then(|f| get_return_stmt_from_stmts(&f.stmts))
+      }),
+    Stmt::While(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::DoWhile(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::For(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::ForIn(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::ForOf(n) => get_return_stmt_from_stmt(&n.body),
+    Stmt::Break(_)
+    | Stmt::Continue(_)
+    | Stmt::Throw(_)
+    | Stmt::Debugger(_)
+    | Stmt::Decl(_)
+    | Stmt::Expr(_)
+    | Stmt::Empty(_) => None,
+  }
 }
