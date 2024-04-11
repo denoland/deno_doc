2ac7c427de7272f573ba8925f4a811ca791075f7
denobot <denobot@users.noreply.github.com>
2024-04-11 20:43:20 +0000

    Publish 8691ce90b8ea5a25f28c826d9836ea5847c6dfc2

--- deno_doc_wasm.generated.js
+++ deno_doc_wasm.generated.js
@@ -424,12 +424,12 @@ const imports = {
     __wbg_set_841ac57cff3d672b: function (arg0, arg1, arg2) {
       getObject(arg0)[takeObject(arg1)] = takeObject(arg2);
     },
-    __wbindgen_string_new: function (arg0, arg1) {
-      const ret = getStringFromWasm0(arg0, arg1);
-      return addHeapObject(ret);
-    },
     __wbg_new_9fb8d994e1c0aaac: function () {
       const ret = new Object();
+      return addHeapObject(ret);
+    },
+    __wbindgen_string_new: function (arg0, arg1) {
+      const ret = getStringFromWasm0(arg0, arg1);
       return addHeapObject(ret);
     },
     __wbindgen_number_new: function (arg0) {
@@ -660,8 +660,8 @@ const imports = {
       const ret = getObject(arg0).queueMicrotask;
       return addHeapObject(ret);
     },
-    __wbindgen_closure_wrapper546: function (arg0, arg1, arg2) {
-      const ret = makeMutClosure(arg0, arg1, 227, __wbg_adapter_48);
+    __wbindgen_closure_wrapper549: function (arg0, arg1, arg2) {
+      const ret = makeMutClosure(arg0, arg1, 226, __wbg_adapter_48);
       return addHeapObject(ret);
     },
   },
