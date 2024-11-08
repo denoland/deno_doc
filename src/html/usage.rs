523e7ea99bc4ac21c3369f9d12f23e88c65b5bce
c43721 <rlahman.game@gmail.com>
2024-11-08 14:49:36 -0600

    fix: proper formatting

--- src/html/usage.rs
+++ src/html/usage.rs
@@ -10,8 +10,8 @@ use serde::Serialize;
 use std::borrow::Cow;
 
 lazy_static! {
-+  static ref IDENTIFIER_RE: Regex =
-+    Regex::new(r"^[0-9]|[^a-zA-Z0-9$_]").unwrap();
+  static ref IDENTIFIER_RE: Regex =
+    Regex::new(r"^[0-9]|[^a-zA-Z0-9$_]").unwrap();
 }
 
 fn render_css_for_usage(name: &str) -> String {
