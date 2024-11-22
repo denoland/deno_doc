6316e8e7a3d6a29965b5739afe29a8b7eabc770e
crowlkats <crowlkats@toaxl.com>
2024-11-22 09:09:49 +0100

    fix

--- tests/testdata/multiple/a.ts
+++ tests/testdata/multiple/a.ts
@@ -2,21 +2,6 @@
  * Some docs
  * with a line break
  *
- * > [!NOTE]
- * > Useful information that users should know, even when skimming content.
- *
- * > [!TIP]
- * > Helpful advice for doing things better or more easily.
- *
- * > [!IMPORTANT]
- * > Key information users need to know to achieve their goal.
- *
- * > [!WARNING]
- * > Urgent info that needs immediate user attention to avoid problems.
- *
- * > [!CAUTION]
- * > Advises about risks or negative outcomes of certain actions.
- *
  * @module
  */
 
