f194955b41a48e61e5fc176926b07eab721dcb59
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-02-09 19:30:43 +0100

    polish contrast ratio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,23 @@ const TAG_CYAN = "#0CAFC6";
 
 const extraColors = {
   "Function": "#056CF0",
+  "FunctionDark": "#4cc3ff",
   "Method": "#056CF0",
+  "MethodDark": "#4cc3ff",
   "Variable": "#7E57C0",
+  "VariableDark": "#b37feb",
   "Property": "#7E57C0",
+  "PropertyDark": "#b37feb",
   "Class": "#20B44B",
+  "ClassDark": "#87eea4",
   "Enum": "#22ABB0",
+  "EnumDark": "#3fced1",
   "Interface": "#D2A064",
+  "InterfaceDark": "#bb733b",
   "TypeAlias": "#A4478C",
+  "TypeAliasDark": "#dd95cc",
   "Namespace": "#D25646",
+  "NamespaceDark": "#e57e6b",
 
   "new": TAG_PURPLE,
   "abstract": TAG_CYAN,
