692710c5036f07aa33e372fb4fc2f9bdc44cc36d
Leo Kettmeir <crowlkats@toaxl.com>
2025-12-25 14:15:44 +0100

    fix(html): hide class constructors if they are private or internal (#751)

--- src/html/symbols/class.rs
+++ src/html/symbols/class.rs
@@ -5,6 +5,7 @@ use crate::html::parameters::render_params;
 use crate::html::render_context::RenderContext;
 use crate::html::types::render_type_def_colon;
 use crate::html::util::*;
+use crate::js_doc::JsDocTag;
 use deno_ast::swc::ast::Accessibility;
 use deno_ast::swc::ast::MethodKind;
 use serde::Serialize;
@@ -116,6 +117,11 @@ fn render_constructors(
 
   let items = constructors
     .iter()
+    .filter(|constructor| {
+      !constructor.js_doc.tags.contains(&JsDocTag::Private)
+        && !constructor.js_doc.tags.contains(&JsDocTag::Internal)
+        && !matches!(constructor.accessibility, Some(Accessibility::Private))
+    })
     .enumerate()
     .map(|(i, constructor)| {
       let id = IdBuilder::new(ctx.ctx)
